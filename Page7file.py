784e21992da644e362a7fda299088067772bcbba
rexc2020 <70179428+rexc2020@users.noreply.github.com>
2020-09-07 11:43:54 -0400

    Add files via upload

---
+++ Page7file.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+df = pd.read_csv('all_040_in_12.P1.csv')
+
+
+# In[3]:
+
+
+print(df)
+
+
+# In[ ]:
+
+
+
+
